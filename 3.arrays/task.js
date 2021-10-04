badd8d091f8287c5017cc8e9710d08399939473d
Skaf08 <viacheslavnik8@gmail.com>
2021-10-04 15:29:15 +0300

    Initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  if (arr1.length === arr2.length) {
+    result = arr1.every(function check(number,i){return number === arr2[i]}); 
+  } else result = false;
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  resultArr = arr.filter(function check3(number){return number % 3 === 0 }).filter(function checkp(number){return number > 0 }).map(function multiplication(number){return number*10});
   return resultArr; // array
 }
