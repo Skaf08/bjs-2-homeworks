45f991917f60e7f38b6c73eaeeef585ebcbc7f12
Skaf08 <viacheslavnik8@gmail.com>
2021-11-19 15:51:18 +0300

    Secondary commit

--- 7.async/task.js
+++ 7.async/task.js
@@ -5,22 +5,18 @@ class AlarmClock {
     }
 
     addClock (time,callback,id) {
-        if(typeof id != 'undefined') {
-            for(let i = 0; i < this.alarmCollection.length; i++) {
-                if (id == this.alarmCollection[i].id) {
-                 return console.error('same id');;
-                } 
-            }
-            this.alarmCollection.push({id, time, callback});     
-        } else throw new Error('error text');
+        if(typeof id === 'undefined') throw new Error('error text');
+        if (this.alarmCollection.find((arg) => id == arg.id) != undefined) {
+            return console.error('same id');
+        }
+        this.alarmCollection.push({id, time, callback});     
     }
 
     removeClock(id) {
-        for (let i=0; i<this.alarmCollection.length; i++) {
-            if(id == this.alarmCollection[i].id) {
-                this.alarmCollection.splice(i,1);
-                return true;
-            }
+        let idx = this.alarmCollection.findIndex((arg) => id == arg.id);
+        if (idx != -1) {
+            this.alarmCollection.splice(idx,1);
+            return true;
         }
         return false;
     }
@@ -36,18 +32,18 @@ class AlarmClock {
     }
 
     start() {
-        function checkClock(call) { 
+        (call) => { 
             if(call.time == this.getCurrentFormattedTime()) {
                 call.callback();
             }
-            if(this.timerid == null) {
-                 this.timerId = setInterval(() => {
-                     for (let i=0;i<this.alarmCollection.length;i++) {
-                        checkClock(this.alarmCollection[i]);
-                     }
-                 }, 1000);
-            }
         }
+        if(this.timerid == null) {
+            this.timerId = setInterval(() => {
+                for (let i=0;i<this.alarmCollection.length;i++) {
+                   checkClock(this.alarmCollection[i]);
+                }
+            }, 1000);
+       }
     }
 
     stop() {
@@ -67,12 +63,12 @@ class AlarmClock {
     }
 }
 
-let PhoneAlarm = new AlarmClock();
-PhoneAlarm.addClock('07:55', () => console.log ('New day!'), 1);
-PhoneAlarm.addClock('08:00', () => console.log ('New day!!'), 2);
-PhoneAlarm.addClock('08:05', () => console.log ('New day!!!'), 3);
-PhoneAlarm.removeClock(2);
-PhoneAlarm.addClock('23:00', () => console.log ('Lets go sleep'), 4);
-PhoneAlarm.start();
-PhoneAlarm.stop();
-PhoneAlarm.printAlarms();
+let phoneAlarm = new AlarmClock();
+phoneAlarm.addClock('07:55', () => console.log ('New day!'), 1);
+phoneAlarm.addClock('08:00', () => console.log ('New day!!'), 2);
+phoneAlarm.addClock('08:05', () => console.log ('New day!!!'), 3);
+phoneAlarm.removeClock(2);
+phoneAlarm.addClock('23:00', () => console.log ('Lets go sleep'), 4);
+phoneAlarm.start();
+phoneAlarm.stop();
+phoneAlarm.printAlarms();
--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,5 +1,5 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  
 }
 
 
